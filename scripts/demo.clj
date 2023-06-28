147301b59f7017c98685081702907cb6772a9ea9
anshu2185 <anshu2185@gmail.com>
2023-06-28 13:12:47 -0500

    Create demo.clj

---
+++ scripts/demo.clj
@@ -0,0 +1,9 @@
+(ns clojure.examples.hello
+   (:gen-class))
+
+;; Description:This program displays Hello World
+;; Script Name: Clojure Hello World
+(defn Example []
+   (def string1 (slurp "Example.txt"))
+   (println string1))
+(Example)
